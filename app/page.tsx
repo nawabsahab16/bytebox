889fe9a36de488fb8dc42601f21703b9fc448a54
nawabsahab16 <Nawab162004@gmail.com>
2024-11-27 18:42:03 +0530

    feat: add layout, authentication form, and Appwrite client setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
   
 export default function Home() {
   return (
-    <div>
-       <h1> ByteBox : the only storage solution you need </h1>
+    <div className="flex-center h-screen">
+       <h1 className="h1"> 
+        ByteBox : the only storage solution you need 
+      </h1>
     </div>
   );
 }
